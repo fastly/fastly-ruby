0b43ee14c36562d0b414e2f23d4fbbe811dfefab
Simon Wistow <simon@thegestalt.org>
2011-11-03 12:15:57 -0700

    Bump version, add Changes file

--- lib/fastly.rb
+++ lib/fastly.rb
@@ -5,7 +5,7 @@
 # A client library for interacting with the Fastly web acceleration service
 class Fastly
   # The current version of the library
-  VERSION = "0.5"
+  VERSION = "0.6"
 
   require 'fastly/fetcher'
   require 'fastly/client'
