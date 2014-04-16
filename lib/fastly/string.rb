5b476743f6a0c9f0ad5a04e7147c870414d0b4fe
ctdk <jbingham@gmail.com>
2014-04-16 13:54:42 -0700

    Brush up gzip and headers a bit, in-progress documenting options

--- lib/fastly/string.rb
+++ lib/fastly/string.rb
@@ -1,3 +1,5 @@
+# add an underscore method to String, so we can easily convert CamelCase to
+# camel_case for CacheSetting and RequestSetting objects.
 class String
   def underscore
     gsub(/([^A-Z])([A-Z]+)/, '\1_\2').downcase
