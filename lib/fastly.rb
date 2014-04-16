da3e64623b6e5fd1c733dced3d29fd9ce7c3a5a9
ctdk <jbingham@gmail.com>
2014-04-16 15:43:48 -0700

    Document new object methods

--- lib/fastly.rb
+++ lib/fastly.rb
@@ -220,6 +220,26 @@ class Fastly
   # opts must contain service_id, version and name params
 
   ##
+  # :method: create_cache_setting(opts)
+  # opts must contain service_id, version and name params
+
+  ##
+  # :method: create_header(opts)
+  # opts must contain service_id, version and name params
+
+  ##
+  # :method: create_gzip(opts)
+  # opts must contain service_id, version and name params
+
+  ##
+  # :method: create_request_setting(opts)
+  # opts must contain service_id, version and name params
+
+  ##
+  # :method: create_response_object(opts)
+  # opts must contain service_id, version and name params
+
+  ##
   # :method: get_user(id)
   # Get a User
 
@@ -274,6 +294,30 @@ class Fastly
   ## 
   # :method: get_settings(service_id, number, name)
   # Get a Settings
+
+  ## 
+  # :method: get_condition(service_id, number, name)
+  # Get a Condition
+
+  ## 
+  # :method: get_cache_setting(service_id, number, name)
+  # Get a Cache Setting
+
+  ## 
+  # :method: get_gzip(service_id, number, name)
+  # Get a Gzip
+
+  ## 
+  # :method: get_header(service_id, number, name)
+  # Get a Header
+
+  ## 
+  # :method: get_request_setting(service_id, number, name)
+  # Get a Request Setting
+
+  ## 
+  # :method: get_response_object(service_id, number, name)
+  # Get a Response Object
 
   ##
   # :method: update_user(user)
@@ -339,6 +383,31 @@ class Fastly
   # :method: update_vcl(vcl)
   # You can also call
   #    vcl.save!
+
+  ## 
+  # :method: update_cache_setting(cache_setting)
+  # You can also call
+  #    cache_setting.save!
+
+  ## 
+  # :method: update_header(header)
+  # You can also call
+  #    header.save!
+
+  ## 
+  # :method: update_gzip(gzip)
+  # You can also call
+  #    gzip.save!
+
+  ## 
+  # :method: update_request_setting(request_setting)
+  # You can also call
+  #    request_setting.save!
+
+  ## 
+  # :method: update_response_object(response_object)
+  # You can also call
+  #    response_object.save!
 
   ## 
   # :method: update_condition(condition)
@@ -418,6 +487,31 @@ class Fastly
   #    vcl.delete!
 
   ## 
+  # :method: delete_cache_setting(cache_setting)
+  # You can also call
+  #    cache_setting.delete!
+
+  ## 
+  # :method: delete_header(header)
+  # You can also call
+  #    header.delete!
+
+  ## 
+  # :method: delete_gzip(gzip)
+  # You can also call
+  #    gzip.delete!
+
+  ## 
+  # :method: delete_request_setting(request_setting)
+  # You can also call
+  #    request_setting.delete!
+
+  ## 
+  # :method: delete_response_object(response_object)
+  # You can also call
+  #    response_object.delete!
+
+  ## 
   # :method: delete_condition(condition)
   # You can also call
   #    condition.delete!
@@ -479,6 +573,26 @@ class Fastly
   # 
   # Get a list of all conditions
 
+  # :method: list_cache_settings
+  # 
+  # Get a list of all cache settings
+
+  # :method: list_headers
+  # 
+  # Get a list of all headers
+
+  # :method: list_gzips
+  # 
+  # Get a list of all gzips
+
+  # :method: list_request_settings
+  # 
+  # Get a list of all request_settings
+
+  # :method: list_response_objects
+  # 
+  # Get a list of all response_objects
+
   # :method: list_versions 
   # 
   # Get a list of all versions
@@ -537,5 +651,4 @@ class Fastly
     options;
   end
 end
-
 
