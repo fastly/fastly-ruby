2492a3c3634710453b174739d93aba6238d48fc6
Ezekiel Templin <zeke@templ.in>
2014-06-04 17:55:08 -0700

    Add VCL#set_main!
    The Fastly API allows customers to upload multiple customer VCLs. The
    primary ("main") VCL must be designated as such. This causes quite a bit
    of confusion, particularly if the user has experience with our UI.
    
    This is a bandaid, but it:
    * exposes VCL's main attribute (boolean)
    * adds a new method (`VCL#set_main!`) for designating `main`
    
    Example Usage:
    ```
    vcl_name = 'new_vcl'
    vcl_content = File.read("./#{vcl_name}.vcl")
    version.upload_vcl(vcl_name, vcl_content)
    version.vcl(vcl_name).main # => false
    version.vcl(vcl_name).set_main!
    version.vcl(vcl_name).main # => true
    ```

--- lib/fastly/vcl.rb
+++ lib/fastly/vcl.rb
@@ -1,12 +1,18 @@
 class Fastly
   # An internal representation of a Varnish Configuration Language file
   class VCL < BelongsToServiceAndVersion
-    attr_accessor :service_id, :name, :content, :comment
+    attr_accessor :service_id, :name, :content, :comment, :main
     ## 
     # :attr: service_id
     # 
     # The id of the service this belongs to.
     # 
+
+    ##
+    # :attr: name
+    #
+    # The name of the uploaded VCL
+    #
 
     ## 
     # :attr: version
@@ -24,5 +30,21 @@ class Fastly
     # :attr: comment 
     # 
     # a free form comment field
+    #
+
+    ##
+    # :attr: main
+    #
+    # A boolean indicating if some specific VCL is the main VCL
+    #
+
+    ##
+    #
+    # Set VCL as main VCL
+    #
+    def set_main!
+      hash = fetcher.client.put("/service/#{service.id}/version/#{version_number}/vcl/#{name}/main")
+      !hash.nil?
+    end
   end
 end
