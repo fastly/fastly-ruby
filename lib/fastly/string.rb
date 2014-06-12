500ee6f37d23273a667fbd7341e5c1ba34702770
Ezekiel Templin <zeke@templ.in>
2014-06-12 13:24:45 -0700

    Use slightly more straightforward method check

--- lib/fastly/string.rb
+++ lib/fastly/string.rb
@@ -1,7 +1,7 @@
 # add an underscore method to String, so we can easily convert CamelCase to
 # camel_case for CacheSetting and RequestSetting objects.
 class String
-  if !instance_methods(false).include?(:underscore)
+  unless method_defined?(:underscore)
     def underscore
       gsub(/([^A-Z])([A-Z]+)/, '\1_\2').downcase
     end
