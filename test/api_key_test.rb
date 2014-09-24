ebee84a1216f5e5b1aed9d8193ce13ddd56f87a1
Michael May <michael@fastly.com>
2014-09-23 19:17:05 -0700

    move to spec style of testing

--- test/api_key_test.rb
+++ test/api_key_test.rb
@@ -1,46 +1,63 @@
-require 'helper'
+require 'test_helper'
 
-class ApiKeyTest < Fastly::TestCase
-  include CommonTests
+# encapsulates various Fastly API key behaviors
+describe Fastly do #Test < Fastly::TestCase
+  let(:login)  { login_opts :api_key }
+  let(:fastly) { Fastly.new login }
+  let(:client) { fastly.client }
 
-  def setup
-    @opts = login_opts(:api_key)
-    begin
-      @client = Fastly::Client.new(@opts)
-      @fastly = Fastly.new(@opts)
-    rescue Exception => e
-      pp e
-      exit(-1)
+  describe 'client' do
+    it 'should be able to make calls to key-authed endpoints' do
+      customer = client.get('/current_customer')
+      assert_equal login[:customer], customer['name']
+    end
+
+    it 'should NOT be able to make calls to fully-authed endpoints' do
+      assert_raises(Fastly::Error) { # Forbidden
+        user = client.get('/current_user') # requires full auth
+      }
     end
   end
 
-  def test_raw_client
-    user = customer = nil
-    assert_raise(Fastly::Error) {
-      user     = @client.get('/current_user')
-    }
-    assert_equal nil, user
+  describe 'fastly object' do
+    it 'should be able to call key-authed methods' do
+      customer = fastly.current_customer
+      assert customer
+      assert_equal login[:customer], customer.name
+    end
 
-    customer = @client.get('/current_customer')
-    assert customer
-    assert_equal @opts[:customer], customer['name']
+    it 'should NOT be able to call fully-authed methods' do
+      assert_raises(Fastly::FullAuthRequired) {
+        current_user = fastly.current_user
+      }
+    end
   end
 
-  def test_current_user_and_customer
-    current_user  = current_customer = nil
-    assert_raise(Fastly::FullAuthRequired) {
-      current_user = @fastly.current_user
-    }
-    assert_equal nil, current_user
+  describe 'purge by key' do
+    it 'should purge successfully with an api key' do
+      begin
+        service_name = "fastly-test-service-#{get_rand}"
+        service = fastly.create_service(:name => service_name)
+        resp = service.purge_by_key('somekey')
+        assert_equal "ok", resp['status']
+      ensure
+        fastly.delete_service(service)  # cleanup
+      end
+    end
 
-    customer = @fastly.current_customer
-    assert customer
-    assert_equal @opts[:customer], customer.name
+    it 'should raise Fastly::AuthRequired if no api key' do
+      begin
+        service_name = "fastly-test-service-#{get_rand}"
+        service = fastly.create_service(:name => service_name)
+        fastly.client.api_key = nil
+
+        assert_raises Fastly::AuthRequired do # This request requires an API key
+          resp = service.purge_by_key('somekey')
+        end
+      ensure
+        fastly.delete_service(service)  # cleanup
+      end
+    end
   end
 
-  def test_purging
-    #assert @fastly.purge('foo')
-    # TODO Won't work until we get fixtures in Heavenly
-    #assert @fastly.purge_all('foo')
-  end
 end
