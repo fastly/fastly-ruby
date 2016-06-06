4a1e63a33f3dd6c3c3dbca60a5cfeaa930b9f65f
Nat Welch <nat@natwelch.com>
2016-06-06 15:57:05 -0700

    address @mmay's comments in #70

--- lib/fastly/dictionary.rb
+++ lib/fastly/dictionary.rb
@@ -6,6 +6,14 @@ class Fastly
       fetcher.list_dictionary_items(:service_id => service_id, :dictionary_id => id)
     end
 
+    def add_item(key, value)
+      fetcher.create_dictionary_item(service_id: service_id, dictionary_id: id, item_key: key, item_value: value)
+    end
+
+    def delete_item(key)
+      fetcher.delete_dictionary_item(service_id: service_id, dictionary_id: id, item_key: key)
+    end
+
     def self.pluralize
       'dictionaries'
     end
