2207ee53064c86fb6df7ef734ea7b069b25a6c40
Nat Welch <nat@natwelch.com>
2016-06-07 09:12:20 -0700

    Fix warning in code

--- lib/fastly/dictionary.rb
+++ lib/fastly/dictionary.rb
@@ -11,7 +11,7 @@ class Fastly
     end
 
     def update_item(key, value)
-      di = items.select {|di| di.item_key.eql? key }.first
+      di = items.select {|item| item.item_key.eql? key }.first
       if di
         di.item_value = value
         fetcher.update_dictionary_item(di)
