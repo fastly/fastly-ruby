7cc45f894d9555e505874c0124a4ad3b8fedce3f
Katsuyuki Omuro <komuro@fastly.com>
2022-12-15 19:04:09 +0900

    minor updates

--- .github/scripts/pack.sh
+++ .github/scripts/pack.sh
@@ -1,11 +1,11 @@
 # install deps
 bundle install
 
-# tag the version
+# mark version
 sed -i "s/^  VERSION = .*$/  VERSION = '${VERSION}'/" lib/fastly/version.rb
 bundle install
 
-# make a gem (output is always ./pkg)
+# make a gem (output is ./pkg)
 rake build
 
 # save the output filename to env
@@ -13,5 +13,5 @@ PACKAGE_FILENAME=$(cd ./pkg && ls -1 -- *.gem)
 echo "PACKAGE_FILENAME=${PACKAGE_FILENAME}" >> "${GITHUB_ENV}"
 
 # move gem to dist
-mkdir "${GITHUB_WORKSPACE}/dist"
+mkdir -p "${GITHUB_WORKSPACE}/dist"
 mv "./pkg/${PACKAGE_FILENAME}" "${GITHUB_WORKSPACE}/dist/${PACKAGE_FILENAME}"
